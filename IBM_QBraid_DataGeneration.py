ab12569c26a916412b4413a75fd6247f3afc3fc1
Shrey Khater <shrey@Shreys-MBP.lan>
2024-07-22 18:28:15 -0700

    added error handling for jobResults endpoint to handle cases where job was queued or running

--- IBM_QBraid_DataGeneration.py
+++ IBM_QBraid_DataGeneration.py
@@ -14,54 +14,10 @@ import numpy as np
 import os
 from dotenv import load_dotenv
 
-from qiskit_ibm_runtime.exceptions import IBMRuntimeError
+from qiskit_ibm_runtime.exceptions import IBMRuntimeError, RuntimeInvalidStateError
 
 from flask import jsonify
 
-
-'''
-def generate_data(length, numLines, machine=None):
-    load_dotenv()
-    API_KEY = os.getenv('IBM_APIKEY')
-    provider = QiskitRuntimeProvider(API_KEY)
-    
-    HGate_circ = QuantumCircuit(100)
-    for i in range(0, 100):
-        HGate_circ.h(i)
-    HGate_circ.measure_all()
-
-    if not machine:
-        machine = get_least_busy_device()
-    
-    device = provider.get_device(machine)
-    output = 'using device ' + machine + ' (QPU obtained)'
-    print(output)
-
-    pm = generate_preset_pass_manager(optimization_level=1, target=device._backend.target)
-    transpiled_circuit = pm.run(HGate_circ)
-    shot_count = (length*numLines)/100
-    job = device.run(transpiled_circuit, shots=shot_count, memory=True)
-    print('job running')
-
-    result = job.result()
-    rawData = result.measurements()
-    print('job completed')
-
-    unbrokenData = []
-    for shot in rawData:
-        for meas in shot:
-            unbrokenData.append(meas)
-
-    results = []
-    for i in range(0, len(unbrokenData), length):
-        results.append(''.join(map(str, unbrokenData[i:i+length])))
-
-    print({'data': results})
-
-    return results
-    '''
-
-#***implement error handling for all functions and input cases
 
 def launch_job(length=100, numLines=1, machine=None):
     API_KEY = os.getenv('IBM_APIKEY')
@@ -106,8 +62,7 @@ def get_job_status(jobID):
     elif job.status() == JobStatus.DONE:
         output = f'job {jobID} has run succesfully on device {job._backend.name}. Call the /QRNG/JobResults?jobID=[JOB ID] endpoint to get QRNG data in JSON'
     else:
-        output = job.status()
-        #***fix return so that it's a string
+        output = job.status().value
     return output
 
 def get_job_results(jobID):
@@ -117,9 +72,17 @@ def get_job_results(jobID):
         job = provider.job(jobID)
     except:
         return('Job not found. Verify job ID URL parameter or else rerun your job')
+    
+    if job.status() == JobStatus.INITIALIZING or job.status() == JobStatus.QUEUED or job.status() == JobStatus.VALIDATING or job.status() == JobStatus.RUNNING:
+        output = job.status().value + ': check JobStatus endpoint or check again later'
+        return output
+    
     QBraidJob = QiskitJob(jobID, job)
-
-    result = QBraidJob.result()
+    try:
+        result = QBraidJob.result()
+    except RuntimeInvalidStateError as ex:
+        return ex.message
+    
     rawData = result.measurements()
     length = len(rawData[0])
 
